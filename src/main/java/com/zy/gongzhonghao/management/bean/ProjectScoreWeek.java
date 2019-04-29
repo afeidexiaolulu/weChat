452432499f4de468496ebf1a0148e9604b01e905
liangwenjie <lwjemail@163.com>
2019-04-29 11:13:17 +0800

    红黑榜完成，解决关闭项目的bug，每周分数增加进入红黑榜数量字段

--- src/main/java/com/zy/gongzhonghao/management/bean/ProjectScoreWeek.java
+++ src/main/java/com/zy/gongzhonghao/management/bean/ProjectScoreWeek.java
@@ -1,12 +1,17 @@
 package com.zy.gongzhonghao.management.bean;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectScoreWeek {
 
     @TableId(type = IdType.AUTO)
@@ -16,9 +21,13 @@ public class ProjectScoreWeek {
 
     private String  itemNo;
 
-    private String statisticsDate;
+    private Date statisticsDate;
 
     private Float score;
+
+    //项目进入排行榜的次数，不参与数据库映射
+    @TableField(exist = false)
+    private Integer TableNum;
 
     private Date insertTime;
 }
