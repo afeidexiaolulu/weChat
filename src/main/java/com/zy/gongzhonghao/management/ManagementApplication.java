c9d42a5cf8d6a80056926d1e417ce1f9a1e97134
liangwenjie <lwjemail@163.com>
2019-07-02 09:37:05 +0800

    6月24日最终版

--- src/main/java/com/zy/gongzhonghao/management/ManagementApplication.java
+++ src/main/java/com/zy/gongzhonghao/management/ManagementApplication.java
@@ -5,8 +5,6 @@ package com.zy.gongzhonghao.management;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,15 +13,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement    //事务
 @MapperScan("com.zy.gongzhonghao.management.mapper")
 @SpringBootApplication
-public class ManagementApplication extends SpringBootServletInitializer {
+public class ManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-
-        return builder.sources(ManagementApplication.class);
-    }
 
 }
